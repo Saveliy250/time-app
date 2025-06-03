8af8bfdccd06954c0c391328cb11202b6ecc66f6
Saveliy250 <savepo@icloud.com>
2025-06-03 20:10:23 +0300

    authRep

---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ProjectList} from "./components/ProjectList.tsx";
+
+export const MainPage = () => {
+    return (
+        <>
+            <ProjectList/>
+        </>
+    );
+};
+
--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -1,4 +1,5 @@
 export const ROUTES = {
     LOGIN: '/login',
     REGISTRATION: '/register',
+    MAIN: '/',
 }
